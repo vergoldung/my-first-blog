920b05b1d7cf2cb289b6bac2492d8f30edc63239
Eleonore Bernard <eleonore.vergoldung@gmail.com>
2016-08-27 16:18:19 +0200

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,9 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField(default=timezone.now)
     created_date = models.DateTimeField(blank=True, null=True)
-
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+            
     def publish(self):
         self.published_date = timezone.now()
         self.save()
